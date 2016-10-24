0d855145104ffe5b98c256149ee66c14dd1ce2a0
Philipp Schaffrath <github@philippschaffrath.de>
2016-10-24 17:22:30 +0200

    deleted app.php since it is not needed anymore

--- appinfo/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace OCA\Theme\AppInfo;
-
-use OC\Theme;
-
-/** @var Theme\ThemeService $themeService */
-$themeService = \OC::$server->query('ThemeService');
-$themeService->setAppTheme('oc-app-theme');
